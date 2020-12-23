d071c247ed36186744c824f585ac5b0916e292c5
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-23 16:09:01 -0500

    commit tree 740fc9c4b9860e9abe2bbb24fb355b55af76d90b

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+
   def index
     @songs = Song.all
   end
@@ -9,45 +10,45 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.notes.build
+    @song.notes.build
   end
 
   def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
+    @song = Song.create(song_params)
+    params[:song][:notes].values.each do |word|
+      Note.create(content: word, song_id: @song.id)
     end
+    redirect_to @song
+
   end
+  # def edit
+  #   @song = Song.find(params[:id])
+  # end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
+  # def update
+  #   @song = Song.find(params[:id])
 
-  def update
-    @song = Song.find(params[:id])
+  #   @song.update(song_params)
 
-    @song.update(song_params)
+  #   if @song.save
+  #     redirect_to @song
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
+  # def destroy
+  #   @song = Song.find(params[:id])
+  #   @song.destroy
+  #   flash[:notice] = "Song deleted."
+  #   redirect_to songs_path
+  # end
 
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :notes)
   end
 end
 
